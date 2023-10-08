0e84fad6f1382170dea41859a4e7c2e0de82fd76
Filip Soukup <fsoukup@artex-is.cz>
2023-10-08 16:53:48 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO.Packaging;
+
+if (args.Length != 2)
+{
+    Console.Error.WriteLine("Expected 2 arguments: file to extract and extract location");
+    return;
+}
+
+var appFile = args[0];
+var extractPath = args[1];
+
+using var packageStream = File.Open(appFile, FileMode.Open);
+using var package = Microsoft.Dynamics.Nav.CodeAnalysis.Packaging.NavAppPackage.Open(packageStream, allowWrite: false);
+using var content = Package.Open(package.ContentStream, FileMode.Open, FileAccess.Read);
+
+foreach (var part in content.GetParts())
+{
+    Console.WriteLine("Extracting: " + part.Uri.ToString());
+
+    var filePath = System.IO.Path.Join(extractPath, part.Uri.ToString());
+    System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(filePath));
+
+    using var fileStream = File.OpenWrite(filePath);
+    part.GetStream().CopyTo(fileStream);
+}
